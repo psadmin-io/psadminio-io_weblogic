1c6fb46580f9360ef2eacbaaaa9c674a0d8f673e
Eric Bolinger <ericb@ericbisme.net>
2017-08-11 12:26:06 -0600

    Add rspec test for Windows

--- spec/classes/java_options_spec.rb
+++ spec/classes/java_options_spec.rb
@@ -1,9 +1,8 @@
 require 'spec_helper'
 
 describe('io_weblogic::java_options', :type => :class) do
-  let(:params) do
+  let(:default_params) do
     {
-      :ps_config_home  => '/tmp/ps_cfg_home/hcmdmo',
       :pia_domain_name => 'hcmdmo',
       :settings        => {'Xmx' => '1024m' },
     }
@@ -12,12 +11,38 @@ describe('io_weblogic::java_options', :type => :class) do
   context 'when called with parameters on Linux' do
     let(:facts) { { :os => {'name' => 'Linux'} } }
 
+    let :params do
+      default_params.merge({
+        :ps_config_home  => '/tmp/ps_cfg_home/hcmdmo',
+      })
+    end
+
     it { is_expected.to compile.with_all_deps }
 
     it { is_expected.to contain_ini_subsetting("hcmdmo WLS JAVA_OPTIONS_LINUX, Xmx, 1024m")
       .with({
-        'path'                 => "/tmp/ps_cfg_home/hcmdmo/webserv/hcmdmo/bin/setEnv.sh",
-        'setting'              => "JAVA_OPTIONS_LINUX",
+        'path'                 => '/tmp/ps_cfg_home/hcmdmo/webserv/hcmdmo/bin/setEnv.sh',
+        'setting'              => 'JAVA_OPTIONS_LINUX',
+        'subsetting_separator' => ' -',
+        'subsetting'           => 'Xmx',
+        'value'                => '1024m',
+      })
+    }
+  end
+
+  context 'when called with parameters on Windows' do
+    let(:facts) { { :os => {'name' => 'windows'} } }
+
+    let :params do
+      default_params.merge({
+        :ps_config_home => 'C:\temp\ps_cfg_home\hcmdmo',
+      })
+    end
+
+    it { is_expected.to contain_ini_subsetting("hcmdmo WLS JAVA_OPTIONS_WIN, Xmx, 1024m")
+      .with({
+        'path'                 => 'C:\\temp\\ps_cfg_home\\hcmdmo\\webserv\\hcmdmo\\bin\\setEnv.cmd',
+        'setting'              => 'JAVA_OPTIONS_WIN',
         'subsetting_separator' => ' -',
         'subsetting'           => 'Xmx',
         'value'                => '1024m',
