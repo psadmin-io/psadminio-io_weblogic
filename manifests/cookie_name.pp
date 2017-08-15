5214c96a527cedb1249129215d6a4a9151eee354
Eric Bolinger <bolinger@esb.ad.cu.edu>
2017-08-15 14:34:26 -0600

    Syntax fixes in cookie_name to get it working locally.
    Added test manifests for classes that were missing

--- manifests/cookie_name.pp
+++ manifests/cookie_name.pp
@@ -4,16 +4,19 @@ class io_weblogic::cookie_name (
 ) inherits io_weblogic::params {
 
   $pia_domain_list.each |$domain_name, $pia_domain_info| {
+
     $ps_cfg_home_dir = $pia_domain_info['ps_cfg_home_dir']
 
     if (!$pia_cookie_name) {
-      $pia_cookie_name = "${pia_domain_name}-PSJSESSIONID"
+      $pia_cookie_name = "${domain_name}-PSJSESSIONID"
     }
 
-    augeas { "${pia_domain_name} weblogic.xml cookie-name" :
+    notify { "domain: ${domain_name}" :}
+
+    augeas { "${domain_name} weblogic.xml cookie-name" :
       lens    => 'Xml.lns',
-      incl    => "${ps_config_home}/webserv/${pia_domain_name}/applications/peoplesoft/PORTAL.war/WEB-INF/weblogic.xml",
-      context => "/files/${ps_config_home}/webserv/${pia_domain_name}/applications/peoplesoft/PORTAL.war/WEB-INF/weblogic.xml/weblogic-web-app/session-descriptor/cookie-name",
+      incl    => "${ps_cfg_home_dir}/webserv/${domain_name}/applications/peoplesoft/PORTAL.war/WEB-INF/weblogic.xml",
+      context => "/files/${ps_cfg_home_dir}/webserv/${domain_name}/applications/peoplesoft/PORTAL.war/WEB-INF/weblogic.xml/weblogic-web-app/session-descriptor/cookie-name",
       changes => "set #text ${pia_cookie_name}",
     }
   }
