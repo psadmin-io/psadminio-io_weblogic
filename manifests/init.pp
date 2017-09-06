9fedc44da8b89d417d771aac42cfc6056a00d763
Eric Bolinger <bolinger@esb.ad.cu.edu>
2017-09-05 21:05:51 -0600

    whitespace

--- manifests/init.pp
+++ manifests/init.pp
@@ -24,17 +24,17 @@ class io_weblogic (
     case $tools_version {
       /8\.54\.*/: {
         $jce_archive_path = 'http://download.oracle.com/otn-pub/java/jce/7/UnlimitedJCEPolicyJDK7.zip'
-      }   
+      }
       /8\.55\.*/: {
         $jce_archive_path = 'http://download.oracle.com/otn-pub/java/jce/7/UnlimitedJCEPolicyJDK7.zip'
-      }   
+      }
       /8\.56\.*/: {
         $jce_archive_path = 'http://download.oracle.com/otn-pub/java/jce/8/jce_policy-8.zip'
-      }   
+      }
       default: {
         fail("Module ${module_name} requires that you have tools_version defined in your hiera to download JCE from Oracle")
-      }   
-    }   
+      }
+    }
   }
   else {
     $jce_archive_path = $jce_path
@@ -48,15 +48,15 @@ class io_weblogic (
       $fileowner       = $domain_user
       $keystr_set      = 'SET SSL_KEY_STORE_PATH'
       $javaopt_set     = 'SET JAVA_OPTIONS_'
-    }   
-    'AIX': {   
+    }
+    'AIX': {
       $platform = 'AIX'
       $setenv          = 'setEnv.sh'
       $extract_command = 'unzip -o -j %s -d '
       $fileowner       = $psft_install_user_name
       $keystr_set      = 'SSL_KEY_STORE_PATH'
       $javaopt_set     = 'JAVA_OPTIONS_'
-    }   
+    }
     'Solaris': {
       $platform = 'SOLARIS'
       $setenv          = 'setEnv.sh'
@@ -64,7 +64,7 @@ class io_weblogic (
       $fileowner       = $psft_install_user_name
       $keystr_set      = 'SSL_KEY_STORE_PATH'
       $javaopt_set     = 'JAVA_OPTIONS_'
-    }   
+    }
     default: {
       $platform = 'LINUX'
       $setenv          = 'setEnv.sh'
