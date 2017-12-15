3aba34adf0521c75f545b6541b7fa5d66738179f
Eric Bolinger <code@ericbisme.net>
2017-12-15 13:49:22 -0700

    errant commas

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,9 +2,9 @@ class io_weblogic::params {
   $ensure                    = present,
   $java_home                 = hiera('jdk_location')
   $tools_version             = hiera('tools_version')
-  $psft_install_user_name    = 'psadm1',
-  $oracle_install_group_name = 'oinstall',
-  $domain_user               = 'psadm2',
+  $psft_install_user_name    = 'psadm1'
+  $oracle_install_group_name = 'oinstall'
+  $domain_user               = 'psadm2'
   $pskey_passwd              = 'password'
   $cacert_passwd             = 'changeit'
   $pia_domain_list           = undef
