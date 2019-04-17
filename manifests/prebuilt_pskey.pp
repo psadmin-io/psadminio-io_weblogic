f180cb736fdfee7fa2a79b58e1005e8f11b90dec
Dan Iverson <dan@daniverson.com>
2019-04-16 22:56:57 -0500

    Add ensure param

--- manifests/prebuilt_pskey.pp
+++ manifests/prebuilt_pskey.pp
@@ -1,4 +1,5 @@
 class io_weblogic::prebuilt_pskey (
+  $ensure = $io_weblogic::ensure,
   $pia_domain_list = $io_weblogic::pia_domain_list,
   $prebuilt_pskey  = $io_weblogic::prebuilt_pskey,
   $psft_runtime_user_name = $io_weblogic::psft_runtime_user_name,
@@ -11,6 +12,7 @@ class io_weblogic::prebuilt_pskey (
     $pskey_location  = "${ps_cfg_home_dir}/webserv/${domain_name}/piaconfig/keystore/pskey"
 
     file {"pskey-${$pskey_location}":
+      ensure => $ensure,
       source => $prebuilt_pskey,
       path   => $pskey_location,
       owner  => $psft_runtime_user_name,
