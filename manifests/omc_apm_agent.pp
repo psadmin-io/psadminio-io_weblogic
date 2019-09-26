6a2fa5b47fe052209902981de7397f91a574af65
Daniel Iverson <dan@daniverson.com>
2019-09-26 17:34:42 -0500

    Move reg key out of pia_domain_list loop

--- manifests/omc_apm_agent.pp
+++ manifests/omc_apm_agent.pp
@@ -8,20 +8,21 @@ class io_weblogic::omc_apm_agent (
   $apm_reg_key = $io_weblogic::apm_reg_key,
 ) inherits io_weblogic {
 
+  file {"${domain_name}-OMC-RegKey":
+    ensure  => $ensure,
+    path    => "${apm_install_dir}/reg.txt",
+    content => $apm_reg_key,
+    owner   => $psft_runtime_user_name,
+    group   => $oracle_install_group_name,
+    mode    => '0644',
+  }
+  
   $pia_domain_list.each |$domain_name, $pia_domain_info| {
 
     $ps_cfg_home_dir = $pia_domain_info['ps_cfg_home_dir']
     $apm_agent_location  = "${ps_cfg_home_dir}/webserv/${domain_name}"
-
-    file {"${domain_name}-OMC-RegKey":
-      ensure  => $ensure,
-      path    => "${apm_install_dir}/reg.txt",
-      content => $apm_reg_key,
-      owner   => $psft_runtime_user_name,
-      group   => $oracle_install_group_name,
-      mode    => '0644',
-    }
-    -> exec {"${domain_name}-OMC-APMAgent":
+    
+    exec {"${domain_name}-OMC-APMAgent":
       command => "su -c 'cd ${apm_install_dir} && /bin/chmod +x ${apm_install_dir}/ProvisionApmJavaAsAgent.sh && ${apm_install_dir}/ProvisionApmJavaAsAgent.sh -d ${apm_agent_location} -no-prompt -regkey-file ${apm_install_dir}/reg.txt -no-wallet' ${psft_runtime_user_name}",
       creates => "${domain_home}/apmagent",
       path    => "/usr/local/bin/:/bin/",
