c0249b5b861de7724e05b895b97f72c85dbf096b
Dan Iverson <dan@daniverson.com>
2023-12-12 17:14:51 -0600

    Fix lookup call params

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,11 +1,11 @@
 class io_weblogic::params {
   $ensure                    = 'present'
-  $domain_user               = 'psadm2'
+  $domain_user               = lookup('psft_runtime_user_name', undef, undef, 'psadm2')
   $pskey_passwd              = 'password'
   $cacert_passwd             = 'changeit'
   $java_home                 = undef
   $tools_version             = undef
-  $pia_domain_list           = lookup('pia_domain_list', '', '', undef)
+  $pia_domain_list           = lookup('pia_domain_list', undef, undef, '')
   $install_jce               = false
   $trustcacerts              = false
   $standard_java_trust       = false
@@ -13,9 +13,9 @@ class io_weblogic::params {
   $certificates              = undef
   $jce_path                  = undef
   $prebuilt_pskey            = undef
-  $psft_runtime_user_name    = lookup('psft_runtime_user_name', '', '', 'psadm2')
-  $psft_install_user_name    = lookup('psft_install_user_name', '', '', 'psadm1')
-  $oracle_install_group_name = lookup('oracle_install_group_name', '', '', 'oinstall')
+  $psft_runtime_user_name    = lookup('psft_runtime_user_name', undef, undef, 'psadm2')
+  $psft_install_user_name    = lookup('psft_install_user_name', undef, undef, 'psadm1')
+  $oracle_install_group_name = lookup('oracle_install_group_name', undef, undef, 'oinstall')
   $omc_apm_agent             = undef
   $apm_install_dir           = undef
   $apm_reg_key               = undef
